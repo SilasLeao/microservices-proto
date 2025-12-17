bec62d5f8f95c1dd527426e90cc7de8dcffa54e4
Silas Leão <leaosilas@gmail.com>
2025-12-17 11:38:31 -0300

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,33 +1,37 @@
 #!/bin/bash
 
+set -e
+
 # --- Configurações ---
 GITHUB_USERNAME=SilasLeao
 GITHUB_EMAIL=leaosilas@gmail.com
 
-SERVICE_NAME=order
-RELEASE_VERSION=v1.2.3
+SERVICE_NAME=payment
 
-go install google.golang.org/protobuf/cmd/protoc-gen-go@latest 
+# 1. Instala os plugins necessários
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+
 export PATH="$PATH:$(go env GOPATH)/bin"
 
-# CORREÇÃO: Cria o diretório de serviço COMPLETO.
-# Isso garante que 'cd' e 'ls' funcionem mesmo que a geração falhe ou funcione de forma diferente.
+# 2. Cria o diretório de saída do serviço
 mkdir -p golang/${SERVICE_NAME}
 
-# 3. O comando protoc
-# O protoc irá criar os arquivos DENTRO de ./golang/order
-protoc --go_out=./golang \
+# 3. Gera os códigos Go a partir do .proto
+protoc \
+  --go_out=./golang \
   --go_opt=paths=source_relative \
   --go-grpc_out=./golang \
   --go-grpc_opt=paths=source_relative \
- ./${SERVICE_NAME}/*.proto
+  ./payment/*.proto
 
-echo "Generated Go source code files"
+echo "Generated Go source code files:"
 ls -al ./golang/${SERVICE_NAME}
 
+# 4. Inicializa o módulo Go
 cd golang/${SERVICE_NAME}
-go mod init \
- github.com/${GITHUB_USERNAME}/microservices-proto/golang/${SERVICE_NAME} || true
+
+go mod init github.com/${GITHUB_USERNAME}/microservices-proto/golang/${SERVICE_NAME} || true
 go mod tidy || true
 
 echo "Go module initialized for ${SERVICE_NAME}."
